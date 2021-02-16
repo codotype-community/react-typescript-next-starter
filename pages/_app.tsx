2138b6d9a1b9886b5a4eab927f6e2442509a46f3
Alexander Schwartzberg <aeksco@gmail.com>
2021-02-15 23:30:22 -0500

    Added next-seo package + example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,13 @@
 import "../src/styles/tailwind.css";
+import Head from "next/head";
 
 export default ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Head>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    );
 };
--- src/components/HomePage/component.tsx
+++ src/components/HomePage/component.tsx
@@ -1,14 +1,8 @@
 import * as React from "react";
-import Head from "next/head";
 
 export function HomePage() {
     return (
         <div className="p-5">
-            <Head>
-                <title>Home</title>
-                <link rel="icon" href="/favicon.ico" />
-            </Head>
-
             <h1 className="text-2xl">React TypeScript Next.js starter</h1>
         </div>
     );
