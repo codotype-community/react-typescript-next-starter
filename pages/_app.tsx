dee0d6740fec1527c67594bbc0b058215faff70d
Alexander Schwartzberg <aeksco@gmail.com>
2021-02-15 20:30:00 -0500

    Added TailwindCSS + postcss + autoprefixer

---
+++ pages/_app.tsx
@@ -0,0 +1,5 @@
+import "../src/styles/tailwind.css";
+
+export default ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
--- src/components/HomePage/component.tsx
+++ src/components/HomePage/component.tsx
@@ -1,3 +1,14 @@
+import Head from "next/head";
+
 export function HomePage() {
-  return <h1>React TypeScript Next.js starter</h1>;
+  return (
+    <div className="p-5">
+      <Head>
+        <title>Home</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <h1 className="text-2xl">React TypeScript Next.js starter</h1>
+    </div>
+  );
 }
