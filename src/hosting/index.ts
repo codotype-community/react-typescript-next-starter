a86c8609a9396df33a9b3eaabf08d0efb4705c4f
Alexander Schwartzberg <aeksco@gmail.com>
2021-04-15 19:29:12 -0400

    Added Hosting generator

---
+++ src/hosting/index.ts
@@ -0,0 +1,49 @@
+import { GeneratorProps } from "@codotype/core";
+import { getConfig } from "../getConfig";
+
+// "none"
+// "vercel"
+// "netlify"
+// "docker"
+
+const Generator: GeneratorProps = {
+  name: "Nextjs Starter Kit - Hosting files",
+  async write({ runtime, project }) {
+    // Gets hosting config
+    const config = getConfig({ project });
+    const hostingPlatform = config.hostingPlatform;
+
+    // Handle "none" hosting option
+    if (hostingPlatform === "none") {
+      return;
+    }
+
+    // Handle "docker" hosting option
+    if (hostingPlatform === "docker") {
+      await runtime.renderTemplate({
+        src: "Dockerfile",
+        dest: "Dockerfile"
+      });
+      await runtime.renderTemplate({
+        src: ".dockerignore",
+        dest: ".dockerignore"
+      });
+      await runtime.renderTemplate({
+        src: "docker-compose.yml",
+        dest: "docker-compose.yml"
+      });
+    }
+
+    // Handle "netlify" hosting option
+    if (hostingPlatform === "netlify") {
+      await runtime.renderTemplate({
+        src: "netlify.toml",
+        dest: "netlify.toml"
+      });
+    }
+
+    // TODO - handle vercel?
+  }
+};
+
+export = Generator;
